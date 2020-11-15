df1a589f7b0fc5beada638ec1b0861f9c8bccaaa
nMingyu <sung97w@naver.com>
2020-11-15 22:12:47 +0900

    2주차 프로그래밍 개발

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
+import datetime
+
 def print_help():
+
     print (
         '',
         '-----------------',
@@ -26,7 +29,107 @@ def print_shift():
 
 def print_shift_list():
 
-    print ('추가예정') 
+    main()
+    
+
+def getMonthName(month):
+
+    li=['January','February','March','April','May','June',
+
+        'July','August','Setember','October','November','December']
+
+    monthName=li[month-1]
+
+    return monthName
+
+def title(year,month):
+
+    print('    ',getMonthName(month),' ',year)
+
+    print('-'*26)
+
+    print('일  월  화  수  목  금  토')
+
+def getStartDay(year,month):
+
+    d=datetime.date(year,month,1)
+
+    return d.weekday()#월요일 0
+
+def getLastDay(year,month):
+
+    if month==12:
+
+       year=year+1
+
+       month=1
+
+    else:
+
+       month=month+1
+
+    d=datetime.date(year,month,1)  
+
+    t=datetime.timedelta(days=1)
+
+    k=d-t
+
+    return k.day
+
+
+def body(year,month):
+
+    startday=getStartDay(year,month)
+
+    lastday=getLastDay(year,month)
+
+    if startday==6:
+
+        s=1
+
+    else:
+
+        s=startday+2
+
+    c=0
+
+    m=0
+
+    for k in range(6):
+
+        for i in range(7):
+
+            c=c+1
+
+            if c<s:
+
+                print('{:>2}'.format(' '),end='  ')
+
+            else:
+
+                if lastday>m:
+
+                    m=m+1
+
+                    print('{:>2}'.format(m),end='  ')
+
+        print()
+
+    
+
+def Dal(year,month):
+
+    title(year,month)
+
+    body(year,month)
+
+def main():
+
+    year=eval(input('연도를 입력하세요(ex. YYYY):'))
+
+    month=eval(input('월을 입력하세요(1~12):'))
+
+    Dal(year,month)
 
 while True:
     print_help()
@@ -45,7 +148,6 @@ while True:
             print ('잘못 입력했습니다.')
     elif choice == '2':
         print_shift_list()
-        break
     else :
         print ('잘못 입력했습니다.') 
 
